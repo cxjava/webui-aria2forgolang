d259ca04370a44986e22e2f6c6a324a3f4f2f9cb
Charles <cxjava@gmail.com>
2015-09-14 14:47:01 +0800

    First import!

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/gin-gonic/contrib/gzip"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	readConfig()
+}
+
+func main() {
+	r := gin.Default()
+	r.Use(gzip.Gzip(gzip.DefaultCompression))
+
+	html := template.Must(template.New("").Delims("[[", "]]").ParseFiles("./views/index.html"))
+	r.SetHTMLTemplate(html)
+
+	r.Static("/assets", "./assets")
+	r.Static("/d", "./d")
+
+	r.GET(config.HomeUrl, func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	r.Run(config.ListenAddress)
+}
